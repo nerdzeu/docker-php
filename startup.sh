0639746dac4aa16bc198ac7ac2e4c6a56bbb7dc8
Paolo Galeone <nessuno@nerdz.eu>
2015-09-09 15:20:54 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@ if [ ! -d /srv/http/nerdz.eu ]; then
     git clone --recursive https://github.com/nerdzeu/nerdz.eu.git
     cd nerdz.eu
     composer install
+    mkdir /srv/http/nerdz.eu/class/config/
     cp /srv/http/test-db-config.php /srv/http/nerdz.eu/class/config/index.php
     cd ..
     git clone https://github.com/nerdzeu/nerdz-test-db.git
